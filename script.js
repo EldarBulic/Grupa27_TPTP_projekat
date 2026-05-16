b8f5221dfddaba34feac347c8daf918fe984a3ca
EldinMaleskic <eldin.maleskic@fet.ba>
2026-05-17 00:06:45 +0200

    Kreirana funkcija za pamćenje i prebacivanje tamnog režima.
    U ovom dijelu koda implementirana je funkcionalnost za prebacivanje između svijetlog i tamnog režima rada (Dark Mode) nakon što se učita DOM stablo. Kod automatski provjerava prethodni izbor korisnika pohranjen u localStorage i primjenjuje odgovarajući način rada, dok klik na namensko dugme dinamički mijenja klasu na <body> elementu, ažurira ikonu dugmeta (sunce/mjesec) i spašava trenutno stanje kako bi se izbor zadržao i nakon osvježavanja stranice.

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+document.addEventListener("DOMContentLoaded", () => {
 
+   // ==========================================
+    // 1. TAMNI MOD + LOCALSTORAGE
+    // ==========================================
+    const themeBtn = document.getElementById('theme-toggle');
+    const body = document.body;
+
+    if (localStorage.getItem('theme') === 'dark') {
+        body.classList.add('dark-mode');
+        if(themeBtn) themeBtn.innerText = '';
+    }
+
+    if(themeBtn) {
+        themeBtn.addEventListener('click', () => {
+            body.classList.toggle('dark-mode');
+            if (body.classList.contains('dark-mode')) {
+                localStorage.setItem('theme', 'dark');
+                themeBtn.innerText = '';
+            } else {
+                localStorage.setItem('theme', 'light');
+                themeBtn.innerText = '';
+            }
+        });
+    }
+});
